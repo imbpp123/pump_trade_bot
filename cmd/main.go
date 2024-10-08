500e8ed8b30e1432368c7ad4da6cab93a7990a74
Nikolai Kornushkov <imbpp123@gmail.com>
2024-10-08 21:52:03 +0200

    main.go with flow

--- cmd/main.go
+++ cmd/main.go
@@ -4,10 +4,22 @@ import (
 	"context"
 	"fmt"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/gofor-little/env"
 
 	"trade_bot/internal/client"
+	"trade_bot/internal/domain"
+	"trade_bot/internal/types"
+)
+
+const (
+	AmountToSpend float64 = 1.1
+)
+
+var (
+	mexcClient *client.Mexc
 )
 
 func main() {
@@ -21,67 +33,132 @@ func main() {
 		panic(err)
 	}
 
-	mexcClient := client.NewMexc(
+	// mexc client
+	mexcClient = client.NewMexc(
 		env.Get("MEXC_API_KEY", ""),
 		env.Get("MEXC_API_SECRET", ""),
 	)
-	fmt.Println(mexcClient.GetCurrencyPriceTicker(ctx, "BTCUSDT"))
 
-	/*
-		currency := "BTCUSDT"
-		candleRepository := repository.NewCandle()
+	// telegram wait for pump currency
+	currency, timestamp := getPumpCurrency(ctx)
+	currencyUSDT := currency + "USDT"
+	fmt.Printf("crypto is set to %s\n", currencyUSDT)
 
-		candleService := domain.NewCandle(mexcClient, candleRepository)
+	// get price for currency
+	price := getPrice(currencyUSDT)
+	fmt.Printf("crypto price is %f\n", price)
 
-		if err := candleService.StartUpdate(ctx); err != nil {
+	// hehe.. qty that we will pump
+	qty := AmountToSpend / price
+	fmt.Printf("QTY to buy is %f\n", qty)
+
+	// create limit order for given currency
+	buyOrder := types.OrderCreate{
+		Currency: currencyUSDT,
+		Side:     types.OrderSideLong,
+		Type:     types.OrderTypeLimit,
+		Price:    getPrice(currencyUSDT) * 0.95,
+		Quantity: qty,
+	}
+	fmt.Printf("%+v\n", buyOrder)
+	_, err = mexcClient.CreateOrder(ctx, &types.OrderCreate{
+		Currency: currencyUSDT,
+		Side:     types.OrderSideLong,
+		Type:     types.OrderTypeLimit,
+		Price:    price * 1.02,
+		Quantity: qty,
+	})
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("order was created\n")
+
+	// close order and sell everything even if we fail!
+	defer func() {
+		fmt.Println("SELL!")
+
+		// sell and close all orders if any
+		// cancel orders.. no need to buy anymore
+		mexcClient.CancelAllOrders(ctx, currencyUSDT)
+
+		// sell all what we possibly have
+		sellOrder := types.OrderCreate{
+			Currency: currencyUSDT,
+			Side:     types.OrderSideShort,
+			Type:     types.OrderTypeLimit,
+			Price:    getPrice(currencyUSDT) * 0.95,
+			Quantity: getAssetQTY(currency),
+		}
+		fmt.Printf("%+v\n", sellOrder)
+		_, err = mexcClient.CreateOrder(ctx, &sellOrder)
+		if err != nil {
 			panic(err)
 		}
-		candleService.SubscribeForUpdate(ctx, currency)
-		time.Sleep(time.Second)
-		fmt.Println(candleService.GetCandle(ctx, currency))
-	*/
-	/*
-		// run account asset update for repository in background
-		assetService := domain.NewAsset(mexcClient, repository.NewAsset())
-		go func() {
-			if err := assetService.RunUpdate(ctx); err != nil {
-				panic(err)
+		fmt.Printf("order was created\n")
+		fmt.Println("DONE!")
+	}()
+
+	// wait for a moment to sell
+	for {
+		candles, err := mexcClient.GetCurrencyCandles(ctx, currency, types.CandleInterval1m)
+		if err != nil {
+			panic(err)
+		}
+
+		for _, c := range candles {
+			if c.OpenTime.Before(timestamp) && c.CloseTime.After(timestamp) {
+				return c, nil
 			}
-		}()
-
-		// run telegram as chat
-		chanMessageIn := make(types.ChannelMessageIn)
-		appIDStr, err := strconv.Atoi(env.Get("TELEGRAM_APP_ID", ""))
-		if err != nil {
-			panic(err)
 		}
-		tgClient, err := client.NewTelegram(client.TelegramOptions{
-			AppID:    appIDStr,
-			ApiHash:  env.Get("TELEGRAM_API_HASH", ""),
-			Phone:    env.Get("TELEGRAM_PHONE", ""),
-			SQLiteDb: env.Get("TELEGRAM_SQLITE_DB", ""),
-			Context:  ctx,
-		})
-		if err != nil {
-			panic(err)
-		}
-		tgClient.StartRecvMessages(ctx, chanMessageIn)
+	}
+	// ---- WAIT FOR A MOMENT TO SELL ENDED! SEEEEELLLL!!!
+}
 
-		// let's do the pump!
+func getPumpCurrency(ctx context.Context) (string, time.Time) {
+	chanMessageIn := make(types.ChannelMessageIn)
+	appIDStr, err := strconv.Atoi(env.Get("TELEGRAM_APP_ID", ""))
+	if err != nil {
+		panic(err)
+	}
+	tgClient, err := client.NewTelegram(client.TelegramOptions{
+		AppID:    appIDStr,
+		ApiHash:  env.Get("TELEGRAM_API_HASH", ""),
+		Phone:    env.Get("TELEGRAM_PHONE", ""),
+		SQLiteDb: env.Get("TELEGRAM_SQLITE_DB", ""),
+		Context:  ctx,
+	})
+	if err != nil {
+		panic(err)
+	}
+	tgClient.StartRecvMessages(ctx, chanMessageIn)
 
-		// get currency from chat
-		chatService := domain.NewChat()
-		currency, err := chatService.WaitForPumpCurrency(ctx, chanMessageIn)
-		if err != nil {
-			panic(err)
-		}
+	// get currency from chat
+	chatService := domain.NewChat()
+	currency, err := chatService.WaitForPumpCurrency(ctx, chanMessageIn)
+	if err != nil {
+		panic(err)
+	}
 
-		// do the job!
+	tgClient.Stop(ctx)
+	close(chanMessageIn)
 
+	return currency, time.Now()
+}
 
-		// stop everything
-		tgClient.Stop(ctx)
-		close(chanMessageIn)
+func getPrice(currency string) float64 {
+	price, err := mexcClient.GetCurrencyPriceTicker(currency)
+	if err != nil {
+		panic(err)
+	}
 
-	*/
+	return price
+}
+
+func getAssetQTY(currency string) float64 {
+	qty, err := mexcClient.GetAssets(currency)
+	if err != nil {
+		panic(err)
+	}
+
+	return qty
 }
